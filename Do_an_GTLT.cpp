4326e231ebbd30a9457dc38850e93950f46c68f9
ductai26998 <ductai26998@gmail.com>
2020-09-11 23:52:37 +0700

    create project with struct

---
+++ Do_an_GTLT.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct element {
+	char* maVatTu;
+	char* tenVatTu;
+	char* loaiVatTu;
+	char* donViTinh;
+	char* ngayNhap;
+	char* nhaSanXuat;
+	int soLuong;
+	int donGia;
+	int thanhTien;
+	
+};
+
+typedef element *List;
